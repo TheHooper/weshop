c06a540f8dc306a62c0a795ad535c84f2e3b3c2f
Hooper <471236844@qq.com>
2016-03-14 18:35:28 +0800

    spring-boot框架雏形

---
+++ weshop-web/src/main/java/com/hooper/Application.java
@@ -0,0 +1,14 @@
+package com.hooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 47123 on 2016/3/14.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ weshop-web/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hellow</title>
+</head>
+<body>
+welcome
+</body>
+</html>
